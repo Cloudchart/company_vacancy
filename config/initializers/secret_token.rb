c674ffe86af5bf2cd2684a209e5f198541da5eba
Andrey Peresleguine <peresleguine@gmail.com>
2013-12-09 16:29:21 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cloudchart::Application.config.secret_key_base = 'eb04ba846efbd228f79528a7ca2f90635db77f33e93a8773901cecbf822661e7cd7ac65df367b7598b856b6b44458a15f0a334405ea7b840f0a8f49ba9895d1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cloudchart::Application.config.session_store :cookie_store, key: '_cloudchart_session'
