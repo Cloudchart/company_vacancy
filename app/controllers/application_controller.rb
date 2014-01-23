73c07947b3fd7ccfaf56ac303d9b4f9b253f1a8f
Andrey Peresleguine <peresleguine@gmail.com>
2014-01-23 19:12:46 +0400

    added presenters, organized sections rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
+    def present(object, klass = nil)
+      klass ||= "#{object.class}Presenter".constantize
+      klass.new(object, view_context)
+    end
+
 end
--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -6,19 +6,23 @@ module BlocksHelper
     end
   end
 
-    # temporary
-  def last_block?(block)
-    block == Block.where(section: block.section).order(:position).last
-  end
-
   def block_actions(block)
     ''.tap do |content|
+        content << '<br>'
         content << '| '
         content << link_to("#{t('lexicon.destroy')}", block.blockable, method: :delete, data: { confirm: t('messages.are_you_sure') })
         content << ' | '
-        content << link_to('↑', blocks_update_position_path(block_id: block.id, shift: :up ), method: :post) unless block.position == 0
+        content << link_to('↑', blocks_update_position_path(block_id: block.id, shift: :up ), method: :post) unless block.position == 1
         content << ' | '
         content << link_to('↓', blocks_update_position_path(block_id: block.id, shift: :down ), method: :post) unless last_block?(block)      
     end.html_safe
   end
+
+  private
+
+    # temporary
+    def last_block?(block)
+      block == Block.where(section: block.section).order(:position).last
+    end  
+
 end
