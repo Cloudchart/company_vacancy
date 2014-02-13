43fc3ceca8f42f25efc4f28c3b841cff823a161d
Andrey Peresleguine <peresleguine@gmail.com>
2014-02-13 12:48:14 +0400

    edited block images controller, deleted block shared form

--- app/controllers/block_images_controller.rb
+++ app/controllers/block_images_controller.rb
@@ -1,3 +1,19 @@
 class BlockImagesController < ApplicationController
 
+  def create
+    @block_image = BlockImage.new params.require(:block_image).permit(:image, :block_id)
+    @block_image.save!
+  rescue ActiveRecord::RecordInvalid
+  ensure
+    redirect_to @block_image.block.owner if @block_image.block.present?    
+  end
+
+  def update
+    @block_image = BlockImage.find params[:id]
+    @block_image.update_attributes! params.require(:block_image).permit(:image)
+  rescue ActiveRecord::RecordInvalid
+  ensure
+    redirect_to @block_image.block.owner if @block_image.block.present?
+  end  
+
 end
