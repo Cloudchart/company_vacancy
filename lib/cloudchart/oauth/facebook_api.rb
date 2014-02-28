1d08fa6aaa8fd1f1ad80b766d7fd1e820e4467fa
Andrey Peresleguine <peresleguine@gmail.com>
2014-02-28 19:29:05 +0400

    added linkedin oauth provider

--- lib/cloudchart/oauth/facebook_api.rb
+++ lib/cloudchart/oauth/facebook_api.rb
@@ -2,7 +2,7 @@ module Cloudchart::OAuth
   module FacebookAPI
 
     def self.search(token_data, query, type)
-      client = Cloudchart::OAuth.clients[:facebook_api]
+      client = Cloudchart::OAuth.clients[:facebook]
       token = OAuth2::AccessToken.from_hash(client, token_data)
       response = token.get('/search', params: { q: query, type: type })
       ActiveSupport::JSON.decode(response.body)['data']
