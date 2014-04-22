e02eff0900f51f4e12481d49705a6efdfdb7ac7a
Andrey Peresleguine <peresleguine@gmail.com>
2014-04-22 16:16:33 +0400

    bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.2.0'
 set :application,   'cloudorgchart'
 set :repo_url,      'git@github.com:Cloudchart/company_vacancy.git'
 set :linked_files,  %w{config/database.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
+set :linked_dirs,   %w{log tmp/pids tmp/cache tmp/sockets}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
