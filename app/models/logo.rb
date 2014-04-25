46d35a957d56077dfbb1fe34d9f1934909d9dff7
Andrey Peresleguine <peresleguine@gmail.com>
2014-04-25 18:37:29 +0400

    fix

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -5,7 +5,7 @@ class Logo < Image
   validate :validate_minimum_image_size
 
   def validate_minimum_image_size
-    if image.width < 100 && image.content_type != 'image/svg+xml'
+    if image.present? && image.width < 100 && image.content_type != 'image/svg+xml'
       errors.add :image, I18n.t('errors.messages.width_too_small')
     end
   end
