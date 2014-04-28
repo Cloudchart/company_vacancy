053a710ab08e667fa09a8347cc2eda12506501bf
Eugene Kovalev <seanchas@gmail.com>
2014-04-28 17:04:35 +0400

    Activation Completion WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  #before_action :require_properly_named_user!
+  before_action :require_properly_named_user!
 
   rescue_from CanCan::AccessDenied do |exception|
     if request.env["HTTP_REFERER"].present?
