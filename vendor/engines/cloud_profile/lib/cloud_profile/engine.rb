fd2716018326b6acd6c5a4f8507c0b128a7ebfd4
Eugene Kovalev <seanchas@gmail.com>
2014-04-28 13:35:29 +0400

    Removed CloudProfile migrations from main app

--- vendor/engines/cloud_profile/lib/cloud_profile/engine.rb
+++ vendor/engines/cloud_profile/lib/cloud_profile/engine.rb
@@ -1,5 +1,14 @@
 module CloudProfile
   class Engine < ::Rails::Engine
     isolate_namespace CloudProfile
+    
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.path['db/migrate'].expand.each do |path|
+          app.config.path['db/migrate'] << path
+        end
+      end
+    end
+    
   end
 end
