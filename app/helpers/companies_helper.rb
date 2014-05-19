36b2f42575f3ff63db2e6fb8d1cd0b0561574743
Andrey Peresleguine <peresleguine@gmail.com>
2014-05-19 16:09:52 +0400

    improved status helper

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -29,12 +29,12 @@ module CompaniesHelper
       friends_related_to_company = Friend.related_to_company(company.id)
       friends_working_in_company = Friend.working_in_company(company.id)
 
-      user_company_friends_intersection = (current_user.friends & friends_working_in_company).size
-      user_company_friends_of_friends_intersection = (current_user.friends & friends_related_to_company).size
+      user_company_friends_intersection = (current_user.friends & friends_working_in_company)
+      user_company_friends_of_friends_intersection = (current_user.friends & friends_related_to_company)
 
-      if user_company_friends_intersection > 0
+      if user_company_friends_intersection.size > 0
         get_social_relation_weight(user_company_friends_intersection, :friends, company.people.size)
-      elsif user_company_friends_of_friends_intersection > 0
+      elsif user_company_friends_of_friends_intersection.size > 0
         get_social_relation_weight(user_company_friends_of_friends_intersection, :friends_of_friends, company.people.size)
       else
         'none'
@@ -45,11 +45,11 @@ module CompaniesHelper
 
 private
 
-  def get_social_relation_weight(intersection_size, intersection_type, company_people_size)
+  def get_social_relation_weight(intersection, intersection_type, company_people_size)
     company_people_size = company_people_size.to_f
-    intersection_size = intersection_size.to_f
+    intersection_size = intersection.size.to_f
 
-    if intersection_type == :friends
+    relation = if intersection_type == :friends
       if intersection_size / company_people_size >= 0.1
         'strong'
       else
@@ -59,11 +59,13 @@ private
       if intersection_size / company_people_size >= 0.5
         'good'
       elsif intersection_size / company_people_size >= 0.3
-        'insignificant'
+        'small'
       else
-        'weak'
+        'tiny'
       end
     end 
+
+    "#{relation} (#{intersection_type.to_s.humanize.underscore} work here: #{intersection.map(&:full_name).join(', ')})"
   end
 
 end
