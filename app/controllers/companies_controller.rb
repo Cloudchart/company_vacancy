1399e003943f7942976a9a7dbf9f2790abb2a539
Andrey Peresleguine <peresleguine@gmail.com>
2014-08-19 22:46:07 +0400

    added content to company url verification file (postmark zero-attachment issue)

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -134,7 +134,7 @@ private
       file_location = File.join(dir_location, "#{company.humanized_id}.txt")
       unless File.exists?(file_location)
         FileUtils.mkdir_p(dir_location)
-        File.new(file_location, 'w')
+        File.open(file_location, 'w') { |f| f.write('Good luck using CloudChart!') }
       end
 
       UserMailer.company_url_verification(token).deliver
