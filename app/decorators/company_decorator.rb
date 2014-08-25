2da8c1ac45eec53e13c426d010763ad1e70211a7
Andrey Peresleguine <peresleguine@gmail.com>
2014-08-25 14:01:17 +0400

    added about paragraph to og:description

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -1,4 +1,8 @@
 class CompanyDecorator < ApplicationDecorator
   decorates :company
 
+  def about
+    strip_tags(company.blocks.find_by(section: 'about').try(:paragraphs).try(:first).try(:content))
+  end
+
 end
