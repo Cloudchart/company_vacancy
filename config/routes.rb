2ecf6e89c811c7973a65622fbd983e1752f2d9a4
Eugene Kovalev <seanchas@gmail.com>
2015-01-14 11:47:17 +0300

    Pinboards initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -94,6 +94,10 @@ Cloudchart::Application.routes.draw do
   resources :subscriptions, only: [:create, :update, :destroy]
   resources :comments, only: [:create, :update, :destroy]
   resources :roles, only: [:update, :destroy]
+  
+  
+  resources :pinboards
+
 
   # Custom
   # 
