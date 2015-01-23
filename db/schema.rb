5eca2c22601c08ba0cd6aab8860b5c9220485899
Andrey Peresleguine <peresleguine@gmail.com>
2015-01-23 14:12:08 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122112447) do
+ActiveRecord::Schema.define(version: 20150122155254) do
 
   create_table "activities", primary_key: "uuid", force: true do |t|
     t.string   "action",                                null: false
