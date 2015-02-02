167b0e7d7592afab24a05fb360cfac0252e1b883
Eugene Kovalev <seanchas@gmail.com>
2015-02-03 00:26:37 +0300

    Removed overfetch in Pinboards Controller

--- app/controllers/pinboards_controller.rb
+++ app/controllers/pinboards_controller.rb
@@ -1,59 +1,59 @@
 class PinboardsController < ApplicationController
-  
-  
-  def index
-    respond_to do |format|
-      format.html
-      format.json { @pinboards = (effective_user.pinboards + Pinboard.general) }
-    end
-  end
-  
 
-  def show
-    @pinboard = effective_user.pinboards.find(params[:id]) rescue Pinboard.general.find(params[:id])
-    
+
+  def index
     respond_to do |format|
       format.html
       format.json
     end
   end
-  
-  
+
+
+  def show
+    @pinboard = effective_user.pinboards.find(params[:id]) rescue Pinboard.general.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+
   def create
     pinboard = effective_user.pinboards.create!(params_for_create)
-    
+
     respond_to do |format|
       format.json { render json: { id: pinboard.uuid } }
     end
-  
+
   rescue ActiveRecord::RecordInvalid
 
     respond_to do |format|
       format.json { render json: :fail, status: 422 }
     end
   end
-  
-  
+
+
   def update
     pinboard = effective_user.pinboards.find(params[:id])
-    
+
     pinboard.update!(params_for_update)
-    
+
     respond_to do |format|
       format.json { render json: { id: pinboard.uuid }}
     end
-  
+
   rescue ActiveRecord::RecordInvalid
 
     respond_to do |format|
       format.json { render json: :fail, status: 422 }
     end
   end
-  
-  
+
+
   def destroy
     pinboard = effective_user.pinboards.find(params[:id])
-    
+
     pinboard.destroy
 
     respond_to do |format|
@@ -63,21 +63,21 @@ class PinboardsController < ApplicationController
 
 
   private
-  
-  
+
+
   def effective_user
     current_user
   end
-  
+
 
   def params_for_create
     params.require(:pinboard).permit(:title, :access_rights)
   end
 
-  
+
   def params_for_update
     params_for_create
   end
 
-  
+
 end
