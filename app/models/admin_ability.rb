d369c8420c53008b5cec0c87e1f44b5892f83ddb
Andrey Peresleguine <peresleguine@gmail.com>
2015-02-04 23:47:24 +0300

    temp fix for system roles assignment

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -7,15 +7,15 @@ class AdminAbility
       can :dashboard
 
       can :read, Person
-      can :read, User
+      can [:read, :update], User
       can [:read, :create], Story
       can [:read, :create], Pinboard
 
       can :manage, [Feature, Interview, Page, Tag, Token]
 
-      can :update, User do |u|
-        !u.is_admin?
-      end
+      # can :update, User do |u|
+      #   !u.is_admin?
+      # end
 
       can [:update, :destroy], Pinboard do |pinboard|
         pinboard.user_id.blank?
