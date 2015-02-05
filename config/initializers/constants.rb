32c19f29d89d6960bd50d859802e9622b68eddd0
Andrey Peresleguine <peresleguine@gmail.com>
2015-02-05 14:07:32 +0300

    disabled admin checkbox if current user is admin

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,4 @@
 module Cloudchart
-  EDITABLE_ROLES = [:editor, :unicorn].freeze
-  ROLES = ([:admin] + EDITABLE_ROLES).freeze
+  ROLES = [:admin, :editor, :unicorn].freeze
   RAILS_ADMIN_INCLUDED_MODELS = %w(Company Feature User Token Page Person Tag Interview Story Pinboard Role)
 end
