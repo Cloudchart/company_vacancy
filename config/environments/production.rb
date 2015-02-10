9092c6ac830a2971a9f8c49a1c4326385c42cdd3
Andrey Peresleguine <peresleguine@gmail.com>
2015-02-10 19:47:12 +0300

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Cloudchart::Application.configure do
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['ASSET_HOST'] || '//i.cloudchart.co' # TODO: update .env in production
+  config.action_controller.asset_host = ENV['ASSET_HOST']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
