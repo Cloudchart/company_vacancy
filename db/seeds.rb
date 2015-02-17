fa307e8947d481b948725f6940a05a78c71c53aa
Andrey Peresleguine <peresleguine@gmail.com>
2015-02-17 13:09:30 +0300

    created guest user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+if ENV['GUEST_EMAIL'] && CloudProfile::Email.find_by(address: ENV['GUEST_EMAIL']).blank?
+  guest = User.create!(
+    first_name: 'Guest',
+    last_name: 'Guest',
+    emails: [CloudProfile::Email.new(address: ENV['GUEST_EMAIL'])],
+    password: 'theleagueofflavour'
+  )
+
+  puts 'Guest user created'
+end
