96c8bf450f0954af87c0037073ae114d302d0752
Andrey Peresleguine <peresleguine@gmail.com>
2015-03-06 14:13:15 +0300

    added validation to posts_story

--- app/models/posts_story.rb
+++ app/models/posts_story.rb
@@ -4,6 +4,8 @@ class PostsStory < ActiveRecord::Base
   belongs_to :story, counter_cache: true
   belongs_to :post
 
+  validates :story, uniqueness: { scope: :post }
+
   def company
     post.company
   end
