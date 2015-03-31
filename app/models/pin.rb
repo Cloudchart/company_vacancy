0607fd7e098faa089aadd9a0d5e32bd62b8d8971
Andrey Peresleguine <peresleguine@gmail.com>
2015-03-31 14:17:25 +0300

    cleaned pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,28 +2,23 @@ class Pin < ActiveRecord::Base
   include Uuidable
   include Trackable
 
-  belongs_to  :user
-  belongs_to  :parent,    class_name: Pin
-  belongs_to  :pinboard
-  belongs_to  :pinnable,  polymorphic: true
+  belongs_to :user
+  belongs_to :parent, class_name: 'Pin'
+  belongs_to :pinboard
+  belongs_to :pinnable, polymorphic: true
+  belongs_to :post, foreign_key: :pinnable_id
 
-  belongs_to  :post, foreign_key: :pinnable_id, foreign_type: Post
+  has_many :children, class_name: 'Pin', foreign_key: :parent_id
 
-  has_many    :children,  class_name: Pin, foreign_key: :parent_id
-
-
-  validates_presence_of :content, if: :should_validate_content_presence?
-
+  validates :content, presence: true, if: :should_validate_content_presence?
 
   def should_validate_content_presence?
     @update_by.present? && user_id != @update_by.uuid
   end
 
-
   def update_by!(update_by)
     @update_by = update_by
   end
-
 
   def destroy
     Pin.transaction do
