de20c710d43554f4137ea42319f7e0e9c979b512
Andrey Peresleguine <peresleguine@gmail.com>
2015-05-07 18:30:32 +0200

    created tour tokens for all unicorns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507110831) do
+ActiveRecord::Schema.define(version: 20150507162240) do
 
   create_table "activities", primary_key: "uuid", force: true do |t|
     t.string   "action",                                null: false
