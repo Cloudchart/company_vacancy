afd62d8225da2d631d8d62aca65d7a38baeedde1
Andrey Peresleguine <peresleguine@gmail.com>
2015-05-11 15:44:15 +0200

    created twitter handle validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ActiveRecord::Base
 
   validates :full_name, presence: true, if: :should_validate_name?
   validates :invite, presence: true, if: :should_validate_invite?
-  validates :twitter, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :twitter, twitter_handle: true, uniqueness: { case_sensitive: false }, allow_blank: true
 
   # validate :validate_email, on: :create
 
