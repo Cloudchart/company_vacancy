744df47d134252fafbb36e52c6774142a10b288d
Eugene Kovalev <seanchas@gmail.com>
2015-06-17 15:31:31 +0300

    DRYed ability in viewer model

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -2,16 +2,20 @@ class Viewer < User
 
 
   def important_companies(scope = {})
-    ability = Ability.new(scope[:current_user] || self)
-    Company.important.select { |c| ability.can?(:read, c) }
+    Company.important.select { |c| ability(scope).can?(:read, c) }
   end
 
 
 
   def published_companies(scope = {})
-    ability = scope[:current_user_ability] || Ability.new(scope[:current_user] || self)
-    Company.published.select { |c| ability.can?(:read, c) }
+    Company.published.select { |c| ability(scope).can?(:read, c) }
   end
 
+
+  private
+
+    def ability(scope = {})
+      scope[:current_user_ability] ||= Ability.new(scope[:current_user] || self)
+    end
 
 end
