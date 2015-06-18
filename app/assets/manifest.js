37b73b46089327c9c705dc35b521d5ee92ed1de2
Eugene Kovalev <seanchas@gmail.com>
2015-06-18 18:11:38 +0300

    Created manifest js

---
+++ app/assets/manifest.js
@@ -0,0 +1,15 @@
+//
+//
+//= link ./javascripts/application.js
+//= link ./javascript/old_browsers.js
+//
+//
+//= link ./stylesheets/application.css
+//= link ./stylesheets/preview.css
+//= link ./stylesheets/guest.css
+//= link ./stylesheets/old_browsers.css
+//
+//
+//= link_tree ./images
+//
+//
