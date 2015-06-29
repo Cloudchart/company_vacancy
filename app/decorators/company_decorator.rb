37607d32d5b7d9770af73eafa37fc55a16f37912
Andrey Peresleguine <peresleguine@gmail.com>
2015-06-29 17:01:44 +0300

    updated og meta

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -5,11 +5,6 @@ class CompanyDecorator < ApplicationDecorator
     company.name.present? ? company.name : 'Unnamed'
   end
 
-  def og_name
-    prefix = company.name == ENV['SITE_NAME'] ? '' : "#{ENV['SITE_NAME']}: "
-    "#{prefix}#{company.name}"
-  end
-
   def tags
     company.tags.map { |tag| "##{tag.name}" }.join(', ')
   end
