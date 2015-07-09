0ef423736e554e92d2f6b29af62f60d9f94f194e
Andrey Peresleguine <peresleguine@gmail.com>
2015-07-09 16:45:24 +0300

    created origin domains migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707144108) do
+ActiveRecord::Schema.define(version: 20150709133306) do
 
   create_table "activities", primary_key: "uuid", force: true do |t|
     t.string   "action",                                null: false
