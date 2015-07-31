8682e55c34078fcd33691c54b835949ec2592d97
Andrey Peresleguine <peresleguine@gmail.com>
2015-07-31 14:45:38 +0300

    added diffbot analyze constant

--- config/initializers/diffbot.rb
+++ config/initializers/diffbot.rb
@@ -2,4 +2,5 @@
   config.token = ENV['DIFFBOT_TOKEN']
 end
 
- DIFFBOT_ARTICLE = DIFFBOT_CLIENT.article(version: 3)
+DIFFBOT_ARTICLE = DIFFBOT_CLIENT.article(version: 3)
+DIFFBOT_ANALYZE = DIFFBOT_CLIENT.analyze(version: 3)
