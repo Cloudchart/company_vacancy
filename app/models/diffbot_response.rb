a020deddf7a385d980ecb6ae8f0ee1540aa61870
Andrey Peresleguine <peresleguine@gmail.com>
2015-08-20 20:28:58 +0300

    exported tags from diffbot responses through articte api

--- app/models/diffbot_response.rb
+++ app/models/diffbot_response.rb
@@ -5,6 +5,7 @@ class DiffbotResponse < ActiveRecord::Base
   serialize :data
 
   has_many :diffbot_response_owners, dependent: :destroy
+  has_many :pins, through: :diffbot_response_owners, source: :owner, source_type: Pin.name
 
   validates :api, presence: true
   validates :resolved_url, url: true, uniqueness: { scope: :api }
