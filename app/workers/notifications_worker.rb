27b5ad432cc0d1f4849ef5d0cae38d27e911721e
Andrey Peresleguine <peresleguine@gmail.com>
2015-09-14 15:35:54 +0300

    configured notifications worker

--- app/workers/notifications_worker.rb
+++ app/workers/notifications_worker.rb
@@ -1,10 +1,25 @@
 class NotificationsWorker < ApplicationWorker
   include Sidetiq::Schedulable
 
-  recurrence { minutely(15) }
+  sidekiq_options queue: :notifications
+  recurrence { minutely(Cloudchart::INSTANT_NOTIFICATIONS_TIQ) }
 
   def perform(last_occurrence, current_occurrence)
-    puts last_occurrence, current_occurrence
+    Notification.all.each do |notification|
+      notify_follower(notification.user_id) if should_notify_follower?(notification)
+    end
+  end
+
+private
+
+  def should_notify_follower?(notification)
+    notification.updated_at < Cloudchart::INSTANT_NOTIFICATIONS_TIQ.minutes.ago ||
+    notification.updated_at - notification.created_at > Cloudchart::INSTANT_NOTIFICATIONS_MAX_DELAY.minutes
+  end
+
+  def notify_follower(id)
+    user = User.find(id)
+    puts user.full_name
   end
 
 end
