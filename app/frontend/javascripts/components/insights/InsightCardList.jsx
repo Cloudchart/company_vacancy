714f274cb90f2343e1ba6e6da10084941e16add8
Eugene Cheltsov <chill.icp@gmail.com>
2015-09-30 08:44:01 +0300

    wip

--- app/frontend/javascripts/components/insights/InsightCardList.jsx
+++ app/frontend/javascripts/components/insights/InsightCardList.jsx
@@ -5,7 +5,7 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import InsightCard from './InsightCard';
 
 const ANIMATION_DURATION = 750;
-const ANIMATION_DELAY = 1e3 * 3;
+const ANIMATION_DELAY = 1e3 * 4;
 
 
 class InsightCardList extends React.Component {
@@ -19,24 +19,26 @@ class InsightCardList extends React.Component {
   }
 
   componentDidMount () {
-    setTimeout(this._switchInsights.bind(this), ANIMATION_DELAY);
+    // setTimeout(this._switchInsights.bind(this), ANIMATION_DELAY);
+    setTimeout(this._incrementIndex.bind(this), ANIMATION_DELAY);
   }
 
   componentDidUpdate () {
-    setTimeout(this._switchInsights.bind(this), ANIMATION_DELAY);
+    // setTimeout(this._switchInsights.bind(this), ANIMATION_DELAY);
+    setTimeout(this._incrementIndex.bind(this), ANIMATION_DELAY);
   }
 
-  _switchInsights () {
-    let insightsNode = this.refs.insights;
-    Array.prototype.forEach.call(insightsNode.childNodes, (node) => {
-      let [currInsightNode, nextInsightNode] = node.childNodes;
-      let currTop = currInsightNode.getBoundingClientRect().top;
-      let nextTop = nextInsightNode.getBoundingClientRect().top;
-      move(currInsightNode).y(currTop - nextTop).set('opacity', 0).duration(ANIMATION_DURATION).end();
-      move(nextInsightNode).y(currTop - nextTop).set('opacity', 1).duration(ANIMATION_DURATION).end();
-    });
-    setTimeout(this._incrementIndex.bind(this), ANIMATION_DURATION);
-  }
+  // _switchInsights () {
+  //   let insightsNode = this.refs.insights;
+  //   Array.prototype.forEach.call(insightsNode.childNodes, (node) => {
+  //     let [currInsightNode, nextInsightNode] = node.childNodes;
+  //     let currTop = currInsightNode.getBoundingClientRect().top;
+  //     let nextTop = nextInsightNode.getBoundingClientRect().top;
+  //     move(currInsightNode).y(currTop - nextTop).set('opacity', 0).duration(ANIMATION_DURATION).end();
+  //     move(nextInsightNode).y(currTop - nextTop).set('opacity', 1).duration(ANIMATION_DURATION).end();
+  //   });
+  //   setTimeout(this._incrementIndex.bind(this), ANIMATION_DURATION);
+  // }
 
   _incrementIndex () {
     this.setState({
@@ -44,29 +46,34 @@ class InsightCardList extends React.Component {
     });
   }
 
-  renderInsights () {
-    let nextIndex = (this.state.index + 1) % this.props.collections.length;
-    let insightsCurr = this.props.collections[this.state.index].insights.edges;
-    let insightsNext = this.props.collections[nextIndex].insights.edges;
-
-    return insightsCurr.map((insight, i) => {
-      let nextInsight = insightsNext[i];
+  _renderInsightItem (insight, key) {
+    if (!!insight) {
       return (
-        <li className="insights__list-el" key={ insight.node.id }>
-          <InsightCard insight={ insight.node } />
-          <InsightCard insight={ nextInsight.node } />
+        <li className="insights__list-el" key={ key }>
+          <ReactCSSTransitionGroup className="insight-card__animation"
+                                   component="div"
+                                   transitionName="insight-card"
+                                   transitionAppearTimeout={ ANIMATION_DURATION }
+                                   transitionEnterTimeout={ ANIMATION_DURATION }
+                                   transitionLeaveTimeout={ ANIMATION_DURATION }>
+            <InsightCard insight={ insight.node } key={ insight.node.id }/>
+          </ReactCSSTransitionGroup>
         </li>
-      )
-    });
+      );
+    } else {
+      return null;
+    }
   }
 
   render () {
     let currCollection = this.props.collections[this.state.index];
+    let currInsights = this.props.collections[this.state.index].insights.edges;
 
     return (
       <div className="insights">
         <h1 className="content-block__head content-block__head_big insights__head">
-          <ReactCSSTransitionGroup transitionName="insights__head-label"
+          <ReactCSSTransitionGroup className="insights__head-label__animation"
+                                   transitionName="insights__head-label"
                                    transitionEnterTimeout={ ANIMATION_DURATION }
                                    transitionLeaveTimeout={ ANIMATION_DURATION }>
             <a className="through insights__head-label"
@@ -74,8 +81,10 @@ class InsightCardList extends React.Component {
                key={ currCollection.id }>{ currCollection.title }</a>
           </ReactCSSTransitionGroup>
         </h1>
-        <ul className="insights__list" ref="insights">
-          { this.renderInsights() }
+        <ul className="insights__list">
+          { this._renderInsightItem(currInsights[0], 'first') }
+          { this._renderInsightItem(currInsights[1], 'second') }
+          { this._renderInsightItem(currInsights[2], 'third') }
         </ul>
       </div>
     );
